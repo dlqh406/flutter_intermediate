50b15481cd1cba419887cc564be19ae3c4f4aa84
Lee bosung <dlqh406@gmail.com>
2020-08-23 14:25:02 +0900

    drawer  옆으로 열리는 메뉴탭 구현 방법

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -13,19 +13,44 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+          child: ListView(
+            //위에 까지 전부 채워주는 소스
+            padding: EdgeInsets.zero,
+              children: [
+                DrawerHeader(
+                  child:  Text('Header'),
+                  decoration: BoxDecoration(
+                    color: Colors.amber
+                  ),
+                ),
+                ListTile(
+                  title: Text('Item1'),
+                  onTap: (){
+                    // title을 눌렀을 때 실행될 코드를 여기에 작성
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: Text('Item1'),
+                  onTap: (){
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: Text('Item1'),
+                  onTap: (){
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+      ),
+      ),
       appBar: AppBar(
         title: Text('AnimatedContainer'),
       ),
       body: Center(
-        child: AnimatedOpacity(
-          opacity: _visible ? 1.0 : 0.0 ,
-          duration:  Duration(seconds:1),
-          child: Container(
-            width: 200,
-            height: 200,
-            color: Colors.blue,
-          ),
-        ),
+        child: Text("Drawer 예제"),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
