7fc15962dfecea307cc9149df9758d1c1bceb6cc
Lee bosung <dlqh406@gmail.com>
2020-08-23 14:10:56 +0900

    투명도가 바뀌는 애니메이션

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,15 +3,13 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
+
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  double _width = 50;
-  double _height = 50;
-  Color _color = Colors.red;
-  double _borderRadious = 8.0;
+  bool _visible  = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,28 +17,25 @@ class _HomePageState extends State<HomePage> {
         title: Text('AnimatedContainer'),
       ),
       body: Center(
-        child: AnimatedContainer(
-            width: _width,
-            height: _height,
-            decoration: BoxDecoration(color: _color, borderRadius: BorderRadius.circular(_borderRadious)),
-            duration: Duration(seconds: 1),
-            curve: Curves.fastLinearToSlowEaseIn, //여러가지 커브들 있음 Curves. 치면 보
+        child: AnimatedOpacity(
+          opacity: _visible ? 1.0 : 0.0 ,
+          duration:  Duration(seconds:1),
+          child: Container(
+            width: 200,
+            height: 200,
+            color: Colors.blue,
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
           setState(() {
-            final random = Random();
-            _width = random.nextInt(300).toDouble();
-            _height = random.nextInt(300).toDouble();
-            _color = Color.fromRGBO(random.nextInt(256), random.nextInt(256), random.nextInt(256), 1);
-            _borderRadious = random.nextInt(100).toDouble();
-        });},
+            _visible = ! _visible;
+          });},
         child: Icon(Icons.play_arrow),
 
       ),
     );
   }
 }
-
 
