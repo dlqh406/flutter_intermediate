e0a96dc9c0ea20803e926d00237507cc73f21267
Lee bosung <dlqh406@gmail.com>
2020-08-23 14:36:44 +0900

    snackBar:  아래로 팝업 올라오는 애니메이션 구현

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,64 +1,28 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
-
 class HomePage extends StatefulWidget {
 
   @override
   _HomePageState createState() => _HomePageState();
 }
-
 class _HomePageState extends State<HomePage> {
-  bool _visible  = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
-          child: ListView(
-            //위에 까지 전부 채워주는 소스
-            padding: EdgeInsets.zero,
-              children: [
-                DrawerHeader(
-                  child:  Text('Header'),
-                  decoration: BoxDecoration(
-                    color: Colors.amber
-                  ),
-                ),
-                ListTile(
-                  title: Text('Item1'),
-                  onTap: (){
-                    // title을 눌렀을 때 실행될 코드를 여기에 작성
-                    Navigator.pop(context);
-                  },
-                ),
-                ListTile(
-                  title: Text('Item1'),
-                  onTap: (){
-                    Navigator.pop(context);
-                  },
-                ),
-                ListTile(
-                  title: Text('Item1'),
-                  onTap: (){
-                    Navigator.pop(context);
-                  },
-                ),
-              ],
-      ),
-      ),
       appBar: AppBar(
-        title: Text('AnimatedContainer'),
-      ),
-      body: Center(
-        child: Text("Drawer 예제"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          setState(() {
-            _visible = ! _visible;
-          });},
-        child: Icon(Icons.play_arrow),
-
+        title: Text('SnackBar')),
+      body: Builder(
+        builder: (context)=> Center(
+          child: RaisedButton(onPressed: (){
+            final snackBar0 = SnackBar(
+              content: Text('메일이 삭제되었습니다'),
+              action: SnackBarAction(label: '취소', onPressed:(){
+              //눌렀을때 처리 코드
+              }),
+            );
+            Scaffold.of(context).showSnackBar(snackBar0);
+          }, child: Text('Show SnackBar'),),
+        ),
       ),
     );
   }
